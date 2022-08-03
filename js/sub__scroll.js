7d38b8c7d6bb5933b23a7651bbbd14c14f14a983
admin0 <admin@jinh.kr>
2022-08-03 23:17:56 +0900

    2.2.2

--- js/sub__scroll.js
+++ js/sub__scroll.js
@@ -2,16 +2,27 @@ var winnerYOffset;
 $(document).ready(function() {
   // winnerYOffset = $('#winner').offset().top;
 });
-$(window).scroll(function() {
+// $(window).scroll(function() {
+//
+//   if (!$('#winner').hasClass('pinned')) {
+//     winnerYOffset = $('#winner').offset().top;
+//   }
+//
+//   if (winnerYOffset < pageYOffset) {
+//     $('#winner').addClass('pinned shadow');
+//   } else {
+//     $('#winner').removeClass('pinned shadow');
+//   }
+//   // console.log($('#winner').offset().top < pageYOffset);
+// });
 
-  if (!$('#winner').hasClass('pinned')) {
-    winnerYOffset = $('#winner').offset().top;
-  }
+// get the sticky element
+const stickyElm = document.querySelector('#winner')
 
-  if (winnerYOffset < pageYOffset) {
-    $('#winner').addClass('pinned shadow');
-  } else {
-    $('#winner').removeClass('pinned shadow');
+const observer = new IntersectionObserver(
+  ([e]) => e.target.classList.toggle('pinned', e.intersectionRatio < 1), {
+    threshold: [1]
   }
-  // console.log($('#winner').offset().top < pageYOffset);
-});
+);
+
+observer.observe(stickyElm)
